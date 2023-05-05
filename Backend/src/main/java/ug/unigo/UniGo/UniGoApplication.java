9a2b9b2b7f96c712331324ff61305eb77920179f
Łukasz Pisarczyk <lukiereq@gmail.com>
2023-05-05 17:48:20 +0200

    added cors configurer

--- Backend/src/main/java/ug/unigo/UniGo/UniGoApplication.java
+++ Backend/src/main/java/ug/unigo/UniGo/UniGoApplication.java
@@ -2,11 +2,25 @@ package ug.unigo.UniGo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class UniGoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniGoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
