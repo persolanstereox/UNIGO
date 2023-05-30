33755fbcec29afe978fb02f00bee05ca6a19be3f
Piotr Jemielita <persolanstereox@gmail.com>
2023-05-30 17:56:47 +0200

    main branch update

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -1,12 +1,11 @@
 import Description from "./Description";
-import StartSection from "./StartSection"
+import StartSection from "./StartSection";
 
 export default async function Home() {
-
   return (
     <main className="h-3/4">
-      <Description/>
-      <StartSection/>
+      <Description />
+      <StartSection />
     </main>
   );
 }
